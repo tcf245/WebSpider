b3298ad61a685b046f6063dc18c0bd7fabb475ca
Chaofan.Tan <tcf245@gmail.com>
2016-11-30 19:05:48 +0800

    selenium

---
+++ src/main/java/crawl/selenium/ProcessWeixin.java
@@ -0,0 +1,107 @@
+package crawl.selenium;
+
+import crawl.spider.process.Processor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import static crawl.spider.WorkCache.gson;
+import static crawl.spider.WorkCache.result;
+
+/**
+ * Created by BFD_303 on 2016/11/30.
+ */
+public class ProcessWeixin implements Processor,Runnable {
+
+    public String name;
+    public BlockingQueue<String> taskQueue = null;
+    public static final Log LOG = LogFactory.getLog(ProcessWeixin.class);
+
+    public ProcessWeixin(String name, BlockingQueue<String> taskQueue) {
+        this.name = name;
+        this.taskQueue = taskQueue;
+    }
+
+    @Override
+    public void run() {
+        String task = "";
+        try {
+            while (taskQueue.size() > 0) {
+                LOG.info("task queue size is  ----> " + taskQueue.size());
+                task = taskQueue.take();
+                process(task);
+            }
+        } catch(SocketTimeoutException e){
+            try {
+                LOG.info("task connect fail. url is : " + task);
+                taskQueue.put(task);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void process(String task) throws IOException, SocketTimeoutException {
+        // 加载功能直接调用 Jsoup.connect 接口
+        Document doc = Jsoup.connect(task).get();
+        Elements page = doc.select("div#page-content");
+        Map<String,Object> m = new HashMap<String,Object>();
+
+        page.forEach(p -> {
+            String title = p.select("h2#activity-name").text();
+            String content = p.select("div#js_content").text();
+            String author = p.select("span.rich_media_meta").text();
+            String posttime = p.select("em#post-date").text();
+            String viewcnt = p.select("span#sg_readNum3").text();
+            String upcnt = p.select("span#sg_likeNum3").text();
+
+            LOG.info("title is ----> " + title);
+            LOG.info("content is ----> " + content);
+            LOG.info("author is ----> " + author);
+            LOG.info("posttime is ----> " + posttime);
+            LOG.info("viewcnt is ----> " + viewcnt);
+            LOG.info("upcnt is ----> " + upcnt);
+
+
+            m.clear();
+            m.put("title",title);
+            m.put("content",content);
+            m.put("author",author);
+            m.put("posttime",posttime);
+            m.put("viewcnt",viewcnt);
+            m.put("upcnt",upcnt);
+
+            LOG.info("get json result ---->" + gson.toJson(m));
+            result.add(gson.toJson(m));
+        });
+
+        }
+
+    public static void main(String[] args) {
+        String url = "http://mp.weixin.qq.com/s?src=3&timestamp=1480494103&ver=1&signature=2x8j32zrMQpScC20E*fEgQ-Lhu9fn684BZ*wlqPCxT5TIMl6vzcJwRWQZIUNTblZNAaCn1djb9nEvFjHRHreC73ZjFY7EoEE934FLdqJka5AV4YEKM-jSuZQ29qDvTr5bqlbYx7WOVwmGtdTjeVn2U3BrwgAIA0ZEfiqJ0gnoXg=";
+        ProcessWeixin weixin = new ProcessWeixin("weixin",new LinkedBlockingDeque<>());
+
+        try {
+            weixin.process(url);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
--- src/main/java/crawl/selenium/WeixinSpider.java
+++ src/main/java/crawl/selenium/WeixinSpider.java
@@ -1,14 +1,21 @@
 package crawl.selenium;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static crawl.spider.WorkCache.gson;
 
 /**
  * Created by tcf24 on 2016/11/29.
@@ -20,35 +27,64 @@ public class WeixinSpider {
         WebDriver browser = new ChromeDriver();
         browser.get("http://weixin.sogou.com/");
 
-        Thread.sleep(1 * 1000);
+        WebDriverWait wait = new WebDriverWait(browser,1);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("loginBtn")));
         browser.findElement(By.id("loginBtn")).click();
         browser.switchTo().frame(0);
-        Thread.sleep(3000);
-        browser.switchTo().frame("ptlogin_iframe");
+
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("ptlogin_iframe"));
         browser.findElement(By.id("switcher_plogin")).click();
-        Thread.sleep(1000);
-        browser.findElement(By.id("u")).sendKeys("QQ");
-        browser.findElement(By.id("p")).sendKeys("PASSWD");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("login_button")));
+        browser.findElement(By.id("u")).sendKeys("2609607316");
+        browser.findElement(By.id("p")).sendKeys("39433956038167");
         browser.findElement(By.id("login_button")).click();
 
-        Thread.sleep(10000);
-        //wait for element..
-//        new WebDriverWait(browser,500).until();
+        Thread.sleep(10 * 1000);
+
+        //等待登录之后输入关键词
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("upquery")));
         browser.findElement(By.id("upquery")).sendKeys("vivo");
         browser.findElement(By.cssSelector("input.swz")).click();
 
 
         //翻页
-        for (int i = 0; i < 100; i++) {
-           Thread.sleep(5 * 1000);
+        for (int i = 1; i < 200; i++) {
+            //等待下一页按钮出现
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.id("sogou_next")));
+
             WebElement next = browser.findElement(By.id("sogou_next"));
             Actions action = new Actions(browser);
             //点击下一页按钮 解决问题：  Element is not clickable at point
             action.moveToElement(next).click().perform();
             System.out.printf("this is no.%d page , now tap the next button....",i);
             System.out.println();
+
+            parseList(browser);
+            Thread.sleep(7 * 1000);
         }
 
+    }
+
+    private static void parseList(WebDriver browser) {
+        List<WebElement> links =  browser.findElements(By.cssSelector("ul.news-list li div.txt-box h3"));
+        links.forEach( l -> {
+            String title = l.getText();
+            String link = l.findElement(By.tagName("a")).getAttribute("href");
+
+            Map<String,Object> m = new HashMap<String,Object>();
+            m.put("title",title);
+            m.put("link",link);
+            m.put("keyword","vivo");
+
+            try {
+
+                FileUtils.write(new File("target/weixinlist.txt"),gson.toJson(m) + "\n","utf-8",true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
